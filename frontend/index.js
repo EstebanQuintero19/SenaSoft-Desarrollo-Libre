48e98f665fefcef3b7b4eb6a2aed40ac78bed31a
LytAdrian <lytadrian31@gmail.com>
2025-10-22 13:23:15 -0500

    Dependencias

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-const PORT = process.env.PORT || 3000
-
-app.listen(prompt, ()=>{
-    console.log('Hey estoy funcionando en e puerto :',PORT)
-})
